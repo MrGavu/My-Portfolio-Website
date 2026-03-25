364872c498ff19e2904a8210603699eb65b9d454
Joshua Gavu <60307356+MrGavu@users.noreply.github.com>
2026-03-25 21:41:36 +0000

    Update companies.data.js

--- src/data/companies.data.js
+++ src/data/companies.data.js
@@ -7,6 +7,7 @@ import wnpg from '../assets/images/wnpg.png';
 import maoney from '../assets/images/maoney.png';
 import dcl from '../assets/images/dcl.png';
 import dooinngs from '../assets/images/dooinngs.png';
+import transition_star from '../assets/images/transition_star.png';
 
 export const COMPANIES = [
     {
@@ -46,6 +47,10 @@ export const COMPANIES = [
         image: dooinngs
     },
     {
+        alt: 'transition_star',
+        image: transition_star
+    },
+    {
         alt: 'Zentech',
         image: zentech
     },
@@ -82,40 +87,8 @@ export const COMPANIES = [
         image: dooinngs
     },
     {
-        alt: 'Zentech',
-        image: zentech
-    },
-    {
-        alt: 'gigmile',
-        image: gigmile
-    },
-    {
-        alt: 'yi_shipping',
-        image: yi_shipping
-    },
-    {
-        alt: 'eat chat',
-        image: eat_chat
-    },
-     {
-        alt: 'dab',
-        image: dab
-    },
-    {
-        alt: 'wnpg',
-        image: wnpg
-    },
-     {
-        alt: 'maoney',
-        image: maoney
-    },
-     {
-        alt: 'dcl',
-        image: dcl
-    },
-    {
-        alt: 'dooinngs',
-        image: dooinngs
+        alt: 'transition_star',
+        image: transition_star
     },
     {
         alt: 'Zentech',
@@ -152,5 +125,49 @@ export const COMPANIES = [
     {
         alt: 'dooinngs',
         image: dooinngs
+    },
+    {
+        alt: 'transition_star',
+        image: transition_star
+    },
+    {
+        alt: 'Zentech',
+        image: zentech
+    },
+    {
+        alt: 'gigmile',
+        image: gigmile
+    },
+    {
+        alt: 'yi_shipping',
+        image: yi_shipping
+    },
+    {
+        alt: 'eat chat',
+        image: eat_chat
+    },
+     {
+        alt: 'dab',
+        image: dab
+    },
+    {
+        alt: 'wnpg',
+        image: wnpg
+    },
+     {
+        alt: 'maoney',
+        image: maoney
+    },
+     {
+        alt: 'dcl',
+        image: dcl
+    },
+    {
+        alt: 'dooinngs',
+        image: dooinngs
+    },
+    {
+        alt: 'transition_star',
+        image: transition_star
     },
 ]
